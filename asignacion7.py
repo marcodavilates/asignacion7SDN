a7006810b1095b0782a1b5865531f15875bce579
marcodavilates <marcodavilatestani@gmail.com>
2021-03-17 00:08:06 -0400

    test

--- asignacion7.py
+++ asignacion7.py
@@ -4,5 +4,6 @@ from pprint import pprint
 response = requests.post(
     'https://sandboxdnac.cisco.com/dna/system/api/v1/auth/token',
     headers={'Authorization':'Basic ZGV2bmV0dXNlcjpDaXNjbzEyMyE='})
+response.raise_for_status(),
 payload=response.json()
 pprint(payload)
